a0a900e712c51a3806cab7e3151eb7582c995c53
Victor <36003172+VictorGhosh@users.noreply.github.com>
2021-03-16 15:40:00 -0400

    Removed html files

--- CISC124_A4/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"chess","l":"Board"},{"p":"chess","l":"Knight"},{"p":"chess","l":"Location"},{"p":"chess","l":"Tour"},{"p":"chess","l":"TourViewer"}];updateSearchResults();
